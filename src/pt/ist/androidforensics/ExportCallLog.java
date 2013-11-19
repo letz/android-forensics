e69a1106f192e6204b610091bc3b2a93e18fb7a7
Ricardo Leitão <ricardo7rl@gmail.com>
2013-11-19 17:42:23 +0000

    Added views for export contacts and export call log
    Signed-off-by: Ricardo Leitão <ricardo7rl@gmail.com>

--- src/pt/ist/androidforensics/ExportCallLog.java
+++ src/pt/ist/androidforensics/ExportCallLog.java
@@ -2,22 +2,13 @@ package pt.ist.androidforensics;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class ExportCallLog extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	public void _exportContacts(View v) {
-
-	}
-
-	public void _exportCallLog(View v) {
-
+		setContentView(R.layout.export_call_logs);
 	}
 
 }
