a39f8021cb0f941582d7d9dd5ba708447c7fe8d8
Ricardo Leitão <ricardo7rl@gmail.com>
2013-11-26 17:28:25 +0000

    populate listview with contacts (name and phone)
    Signed-off-by: Ricardo Leitão <ricardo7rl@gmail.com>

--- src/pt/ist/androidforensics/ExportContacts.java
+++ src/pt/ist/androidforensics/ExportContacts.java
@@ -1,15 +1,64 @@
 package pt.ist.androidforensics;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ExportContacts extends Activity {
+
+	private ListView mContactsList;
+	private long mContactId;
+	private String mContactKey;
+	private Uri mContactUri;
+	private SimpleCursorAdapter mCursorAdapter;
+	private ArrayList<String> contacts = new ArrayList<String>();
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.export_contacts);
+
+		mContactsList = (ListView) findViewById(R.id.contacts);
+
+		ContentResolver cr = getContentResolver();
+		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+				null, null, null, null);
+		if (cur.getCount() > 0) {
+			while (cur.moveToNext()) {
+				String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+				String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+				if (Integer.parseInt(cur.getString(
+						cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+					Cursor pCur = cr.query(
+							ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+							null,
+							ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+							new String[]{id}, null);
+					while (pCur.moveToNext()) {
+						String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+						contacts.add("Name: " + name + ", Phone No: " + phoneNo);
+						Log.d("Letz", "Name: " + name + ", Phone No: " + phoneNo );
+						//Toast.makeText(this, "Name: " + name + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+					}
+					pCur.close();
+				}
+			}
+		}
+		adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, contacts);
+		mContactsList.setAdapter(adapter);
 	}
+
+
 
 }
 
